81b00a78fea98bac8c224f0c0922f25c5041e5f7
Exaltprecious <peculiarchristj1234@gmail.com>
2024-10-18 01:35:29 +0100

    ]

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,5 @@
-Creates a table with unique users. 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
